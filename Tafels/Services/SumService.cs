d60deaad12780cf3b5a73f1c7dcb82bd1c87b692
Martijn Storck <martijn@storck.io>
2021-03-21 21:40:47 +0100

    Reduce double sums

--- Tafels/Services/SumService.cs
+++ Tafels/Services/SumService.cs
@@ -7,7 +7,7 @@ namespace Tafels.Services
     public class SumService
     {
         private readonly Random _rand = new();
-        private List<Sum> _history = new();
+        private Queue<(int, int)> _history = new();
 
         public List<Sum> Generate(int number, List<int> tables)
         {
@@ -15,19 +15,26 @@ namespace Tafels.Services
 
             for (var i = 0; i < number; i++)
             {
-                var sum = new Sum {A = _rand.Next(10) + 1, B = tables[_rand.Next(tables.Count)]};
-                // Retry once if sum is in history
-                if (_history.Contains(sum))
-                    sum = new Sum {A = _rand.Next(10) + 1, B = tables[_rand.Next(tables.Count)]};
+                Sum sum;
+
+                var tries = 0;
+                do
+                {
+                   sum = new Sum {A = _rand.Next(1, 10), B = tables[_rand.Next(tables.Count)]};
+                } while (_history.Contains((sum.A, sum.B)) && ++tries < 3);
 
                 sums.Add(sum);
-                _history.Add(sum);
+                _history.Enqueue((sum.A, sum.B));
+                FlushHistory(number * 2);
             }
 
-            if (_history.Count > number * 3)
-                _history = _history.GetRange(0, number * 3);
-
             return sums;
+        }
+
+        private void FlushHistory(int keep)
+        {
+            while (_history.Count > keep)
+                    _history.Dequeue();
         }
     }
 }
