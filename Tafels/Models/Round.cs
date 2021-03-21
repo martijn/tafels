2e3ef6fb38d5a114b0516753610ae252a3d5307d
Martijn Storck <martijn@storck.io>
2021-03-21 19:48:03 +0100

    Quiz rounds, some layout

---
+++ Tafels/Models/Round.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace Tafels.Models
+{
+    public class Round
+    {
+        public List<Sum> Sums { get; set; }
+        public bool ShowResults { get; set; }
+    }
+}
--- Tafels/Models/Sum.cs
+++ Tafels/Models/Sum.cs
@@ -1,10 +1,14 @@
 namespace Tafels.Models
 {
-    public record Sum
+    public class Sum
     {
         public int A { get; init; }
         public int B { get; init; }
 
         public int Answer => A * B;
+
+        public int? UserAnswer { get; set; }
+
+        public bool Correct => Answer == UserAnswer;
     }
 }
